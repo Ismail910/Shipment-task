32e5d3bc494227f0b03ede1913215aff7e12cc2e
Ismail Magdy <119896483+Ismail910@users.noreply.github.com>
2024-01-19 00:59:26 +0200

    create structure to task

---
+++ app/Http/Controllers/JournalEntityController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\JournalEntity;
+use App\Http\Requests\StoreJournalEntityRequest;
+use App\Http\Requests\UpdateJournalEntityRequest;
+
+class JournalEntityController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreJournalEntityRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(JournalEntity $journalEntity)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(JournalEntity $journalEntity)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateJournalEntityRequest $request, JournalEntity $journalEntity)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(JournalEntity $journalEntity)
+    {
+        //
+    }
+}
